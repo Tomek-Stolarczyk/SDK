3774942c5c05d2dddf5485bfd822e5cf03a30e10
Tomek-Stolarczyk <Tomek.stolarczyk9@gmail.com>
2018-05-10 22:51:54 -0400

    move window hpp file to include folder

--- SDK/Window.cpp
+++ SDK/Window.cpp
@@ -32,8 +32,8 @@ bool Window::Create(const std::string window_name, int width, int height)
   const DWORD style = WS_OVERLAPPEDWINDOW;
   RECT desktop;
   GetWindowRect(GetDesktopWindow(), &desktop);
-  const int x = desktop.right / 2;
-  const int y = desktop.bottom / 2;
+  const int x = (desktop.right / 2) - (width / 2);
+  const int y = (desktop.bottom / 2) - (height / 2); 
   _handle = CreateWindowExA(0, RegisterName().c_str(), window_name.c_str(), style, x, y,
                width, height, NULL, NULL, GetModuleHandle(NULL), NULL);
   if (_handle == NULL)
